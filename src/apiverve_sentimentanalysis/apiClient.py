73e2aadea113824b1f83ee0c9b2965ce682907b3
APIVerve Team <hello@apiverve.com>
2025-03-28 17:48:28 -0500

    Bug Fixes

--- src/apiverve_sentimentanalysis/apiClient.py
+++ src/apiverve_sentimentanalysis/apiClient.py
@@ -10,10 +10,10 @@ class SentimentanalysisAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/sentimentanalysis'
+        self.base_url = 'https://api.apiverve.com/v1/sentimentanalysis'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
